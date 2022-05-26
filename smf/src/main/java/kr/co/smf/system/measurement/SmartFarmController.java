0f8518ba83bdc7ea143b42512842ebdd62dae2be
ghdgh <ghdghd5560@gmail.com>
2022-05-27 02:21:45 +0900

    photo rendering, measurement rendering, view.jsp fin

--- smf/src/main/java/kr/co/smf/system/measurement/SmartFarmController.java
+++ smf/src/main/java/kr/co/smf/system/measurement/SmartFarmController.java
@@ -1,7 +1,5 @@
 package kr.co.smf.system.measurement;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,13 +36,13 @@ public class SmartFarmController {
 
 	@Autowired
 	private SystemUtil systemUtil;
-	
+
 	@Autowired
 	private PhotoUtil photoUtil;
-	
+
 	@Autowired
 	private Navigator navigator;
-	
+
 	@Autowired
 	private SettingService settingService;
 
@@ -57,13 +56,12 @@ public class SmartFarmController {
 	@GetMapping(path = "/smartfarm", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public List<Map<String, String>> viewSmartFarmList(@RequestParam Map<String, String> condition) {
-		
 		List<Map<String, String>> responseList = new ArrayList<Map<String, String>>();
 
 		List<Agent> agents = agentService.viewAgentInfoList(condition);
 		Map<String, Setting> settings = new ConcurrentHashMap<String, Setting>();
 		Map<String, Measurement> measurements = new ConcurrentHashMap<String, Measurement>();
-		
+
 		for (Agent agent : agents) {
 			Thread thread = new Thread(new Runnable() {
 				@Override
@@ -76,27 +74,20 @@ public class SmartFarmController {
 					}
 				}
 			});
-			
-			//thread.start();		// TODO 에이전트 구현 시 주석 해제
+
+			// thread.start(); // TODO 에이전트 구현 시 주석 해제
 		}
-		
+
 		/*
-		while (true) {
-			if (agents.size() == settings.size() && agents.size() == measurements.size()) {
-				break;
-			} else {
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		}
-		*/		// TODO 에이전트 구현 시 주석 해제
+		 * while (true) { if (agents.size() == settings.size() && agents.size() ==
+		 * measurements.size()) { break; } else { try { Thread.sleep(500); } catch
+		 * (InterruptedException e) { e.printStackTrace(); }
+		 * 
+		 * } }
+		 */ // TODO 에이전트 구현 시 주석 해제
 		for (Agent agent : agents) {
 			Map<String, String> element = new HashMap<String, String>();
-			
+
 			element.put("agentNo", "" + agent.getNo());
 			element.put("agentName", agent.getAgentName());
 			/*
@@ -113,92 +104,109 @@ public class SmartFarmController {
 			 * measurements.get(agent.getAgentIpAddress()).getHumidity());
 			 * element.put("measureCo2", "" +
 			 * measurements.get(agent.getAgentIpAddress()).getCo2());
-			 */		// TODO 에이전트 구현 시 주석 해제
+			 */ // TODO 에이전트 구현 시 주석 해제
 			responseList.add(element);
 		}
-		
+
 		Map<String, String> allCondition = new HashMap<String, String>();
-		
+
 		allCondition.putAll(condition);
-		
+
 		allCondition.remove("pageNo");
-		
+
 		List<Agent> allAgents = agentService.viewAgentInfoList(allCondition);
-		
-		String navigatorHtml = navigator.getNavigator(
-				allAgents.size(), Integer.parseInt(condition.get("pageNo")));
-		
+
+		String navigatorHtml = navigator.getNavigator(allAgents.size(), Integer.parseInt(condition.get("pageNo")));
+
 		if (responseList.isEmpty()) {
 			responseList.add(new HashMap<String, String>());
 		}
-		
+
 		responseList.get(0).put("navigator", navigatorHtml);
-		
+
 		return responseList;
 	}
 
 	@GetMapping("/smartfarm/{no}")
 	public ModelAndView viewSmartFarm(Agent agent) {
 		ModelAndView mav = new ModelAndView("smartfarm/view");
-		
+
 		agent = agentService.viewAgentInfo(agent);
 		mav.addObject(agent);
 		/*
 		 * try { mav.addObject("setting", systemUtil.requestRealTimeGrowthInfo(agent));
 		 * } catch (IOException e) { e.printStackTrace(); // TODO 예외 처리 }
 		 */
-		
-		
+
 		//
 		Setting setting = new Setting();
 		setting.setUserPhoneNumber("01051199268");
 		setting.setSettingName("느타리버섯");
 		mav.addObject("setting", settingService.viewSetting(setting));
-		
+
 		// TODO 모데이터... 삭제 예정
-		
-		
+
 		Map<String, String> condition = new HashMap<String, String>();
 		condition.put("userPhoneNumber", agent.getUserPhoneNumber());
-		
+
 		mav.addObject("settings", settingService.viewSettingList(condition));
-		
+
 		return mav;
 	}
 
 	@PutMapping("/smartfarm")
 	public Agent editSmartFarm(Agent agent) {
-		System.out.println("put!!");
-		
-		Map<String, String> condition  = new HashMap<String, String>();
+		Map<String, String> condition = new HashMap<String, String>();
 		condition.put("previousAgentIpAddress", agent.getAgentIpAddress());
+		condition.put("nowAgentIpAddress", agent.getAgentIpAddress());
 		condition.put("agentName", agent.getAgentName());
-		
+
 		agentService.editAgentInfo(condition);
-		
-		return agent;		//TODO 혹시 문제가 생길 경우 조회한 다음 조회한 결과를 반환할 것
+
+		return agent;
 	}
 
 	@PutMapping("/control")
 	public void controlSmartFarm(Agent agent, Setting setting) {
+		System.out.println(">>>>>>>>>control : " + agent + setting);
+
 		try {
 			systemUtil.requestControlGrowth(agent, setting);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 
-	@GetMapping(path = "/measurement", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@GetMapping(path = "/measurement")
 	@ResponseBody
-	public List<Measurement> viewMeasureInfo(@RequestBody Map<String, String> condition) {
+	public List<Measurement> viewMeasureInfo(@RequestParam Map<String, String> condition) {
+		System.out.println("view >>> " + condition + "list : " + measurementService.viewMeasurementList(condition));
+
 		return measurementService.viewMeasurementList(condition);
+	}
+
+	@PostMapping(path = "/measurement", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public Map<String, String> addMeasureInfo(@RequestBody Measurement measurement) {
+		Map<String, String> response = new HashMap<String, String>();
+		
+		if (measurementService.addMeasurement(measurement)) {
+			response.put("code", "200");
+			response.put("message", "ok");
+		} else {
+			response.put("code", "300");
+			response.put("message", "fail");
+		}
+		
+		return response;
 	}
 
 	@GetMapping(path = "/photo")
 	@ResponseBody
 	public Map<String, String> viewPhoto(@RequestParam Map<String, String> condition) {
 		Map<String, String> response = photoUtil.selectPhoto(condition);
-		
+
 		return response;
 	}
 }
